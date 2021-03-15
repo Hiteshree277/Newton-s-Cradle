1b4c33dc7b1c92e78eaff0896ee5c51a995f29db
Hiteshree <73422564+Hiteshree277@users.noreply.github.com>
2021-03-15 19:04:58 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,26 @@
+class Bob{
+    constructor(x,y){
+        var options={
+            restitution:1,
+            isStatic:false,
+            friction:0.3,
+            density:0.8
+        }
+        this.body=Matter.Bodies.circle(x,y,25,options);
+        this.radius=25;
+        
+        World.add(world,this.body);
+
+    }
+
+    display(){
+        var pos=this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        fill(255);
+        stroke("lightblue");
+        //ellipseMode(RADIUS);
+        ellipse(0,0,this.r*2,this.r*2);
+        pop();
+    }
+}
